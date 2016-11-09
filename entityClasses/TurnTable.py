41d89b6e778a3b05619c1bfc28641a75b1e25f04
bshappell <bshappell@wpi.edu>
2016-11-09 22:09:34 +0000

    working stepper motor increment code

--- entityClasses/TurnTable.py
+++ entityClasses/TurnTable.py
@@ -1,21 +1,36 @@
 # TurnTable Class
 
 import RPi.GPIO as GPIO
+from time import sleep
 
 class TurnTable(object):
 
-	def __init__(self, stepPin, dirPin):
+	def __init__(self, stepPin):
 
 		self.stepPin = stepPin
-		self.dirPin = dirPin
+		GPIO.setmode(GPIO.BCM)
+		#GPIO.setwarnings(False)
+		GPIO.setup(self.stepPin, GPIO.OUT)
 
 	""" Increment the turntable one step """
 	def increment(self):
 
-		# increment the stepper motor once
-		pass
+		# set the pin high and then low again
+		GPIO.output(self.stepPin, GPIO.HIGH)
+		GPIO.output(self.stepPin, GPIO.LOW)
 
 	""" Increment the turntable one blade """
 	def incrementBlade(self, currStage):
 
 		pass
+
+
+
+
+if __name__=="__main__":
+	
+	myTurn = TurnTable(21)
+
+	while(1):
+		myTurn.increment()
+		sleep(0.007)
