53a2179b3d0264d4e1295ad490166cee6e0dcf04
bshappell <bshappell@wpi.edu>
2017-01-16 21:27:59 +0000

    Modified Blisk and Stage classes

--- entityClasses/Blisk.py
+++ entityClasses/Blisk.py
@@ -2,8 +2,22 @@
 
 class Blisk(object):
 
-	def __init__(self, stages, heightBetweenStages, inspectionTime):
+	def __init__(self, bliskID, inspectionTime, firstStage, secondStage):
 
-		self.stages = stages
-		self.heightBetweenStages = heightBetweenStages
+                self.bliskID = bliskID
 		self.inspectionTime = inspectionTime
+		self.firstStage = firstStage
+		self.secondStage = secondStage
+
+	def getBliskID(self):
+                return self.bliskID
+
+	def getFirstStage(self):
+                return self.firstStage
+
+        def getSecondStage(self):
+                return self.secondStage
+
+        def getInspectionTime(self):
+                return self.inspectionTime
+		
--- entityClasses/Stage.py
+++ entityClasses/Stage.py
@@ -3,11 +3,17 @@
 
 class Stage(object):
 
-	def __init__(self, numberBlades, filletRadius, distBetweenBlades, largeBBRadius, smallBBRadius):
+	def __init__(self, numberBlades, largeBBRadius, smallBBRadius):
 
 		self.numberBlades = numberBlades
-		self.filletRadius = filletRadius
-		self.distBetweenBlades = distBetweenBlades
 		self.largeBBRadius = largeBBRadius
 		self.smallBBRadius = smallBBRadius
 
+	def getNumberBlades(self):
+                return self.numberBlades
+
+        def getLargeBBRadius(self):
+                return self.largeBBRadius
+
+        def getSmallBBRadius(self):
+		return self.smallBBRadius
