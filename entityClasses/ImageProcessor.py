a5a4f0196799e99929258355a9328f2a88e51dcd
bshappell <bshappell@wpi.edu>
2017-04-08 14:50:59 -0400

    cleanup of image processor and added bb inspection code

--- entityClasses/ImageProcessor.py
+++ entityClasses/ImageProcessor.py
@@ -5,10 +5,12 @@ import InspectionResults
 import InspectionPosition
 import sys
 import time
+import Shapes
 
 DEBUG = 0 # Toggle to get debug features
-RASP_PI = 1 # Indicates whether the code is running on the Raspberry Pi or not
+RASP_PI = 0 # Indicates whether the code is running on the Raspberry Pi or not
 CAMERA = 1 # Indicates whether to run the code with the camera
+VIDEO = 0 # Indicates if a video should be recorded
 
 HUE_LOW = 26
 HUE_HIGH = 71
@@ -17,10 +19,19 @@ SATURATION_HIGH = 255
 VALUE_LOW = 116 #116
 VALUE_HIGH = 255
 
+Y_MAX = 200
+Y_MIN = 130
+X_MAX = 420
+X_MIN = 300
+MIN_RAD_SMALL = 50
+MAX_RAD_SMALL = 70 
+MIN_RAD_LARGE = 70
+MAX_RAD_LARGE = 80
+WEIGHT = 7 
+
 class ImageProcessor(object):
 
 	def __init__(self):
-
 
 		if RASP_PI:
 			self.capture = cv2.VideoCapture(0)
@@ -36,110 +47,10 @@ class ImageProcessor(object):
 		self.upper_green = np.array([HUE_HIGH,SATURATION_HIGH,VALUE_HIGH])
 
 		""" The Different Boxes to search for light in """
-		"""self.box1 = Box(80,80,100,100)
-		self.box2 = Box(50,270,100,100)
-		self.box3 = Box(250,320,100,100)"""
-		self.box1 = Box(400,85,70,70,True)
-		self.box2 = Box(285,50,100,50,True)
-		self.box3 = Box(220,140,70,70,True)
-
-		""" Array of Test Images """
-		"""frame1 = cv2.imread('..\\better_pics\\Up2Covered.jpg',-1)
-		frame2 = cv2.imread('..\\better_pics\\Up2Uncovered.jpg',-1)
-		frame3 = cv2.imread('..\\better_pics\\Down1Covered.jpg',-1)
-		frame4 = cv2.imread('..\\better_pics\\Down1Uncovered.jpg',-1)
-		frame5 = cv2.imread('..\\better_pics\\Down2Covered.jpg',-1)
-		frame6 = cv2.imread('..\\better_pics\\Down2Uncovered.jpg',-1)
-		frame7 = cv2.imread('..\\better_pics\\Up1Uncovered.jpg',-1)
-		frame8 = cv2.imread('..\\better_pics\\Up1Covered.jpg',-1)"""
+		self.box1 = Shapes.Box(400,85,70,70,True)
+		self.box2 = Shapes.Box(285,50,100,50,True)
+		self.box3 = Shapes.Box(220,140,70,70,True)
 		
-		frame1 = cv2.imread('../pictures/Large_Fail.png',-1)
-		frame2 = cv2.imread('../pictures/Large_Pass.png',-1)
-		frame3 = cv2.imread('../pictures/Plain_Large.png',0)
-		frame4 = cv2.imread('../pictures/Plain_Small.png',0)
-
-		frame5 = cv2.imread('../better_pics/Up2Covered.jpg', 0)
-                frame6 = cv2.imread('..better_pics/Up2Uncovered.jpg',0)
-                frame7 = cv2.imread('../better_pics/Up1Uncovered.jpg',0)
-                frame8 = cv2.imread('../better_pics/Up1Covered.jpg',0)
-		
-		"""frame5 = cv2.imread('../pictures/Small_Fail.png',-1)
-		frame6 = cv2.imread('../pictures/Small_Pass.png',-1)
-		frame7 = cv2.imread('../pictures/Small_Pass_Light.png',-1)
-		frame8 = cv2.imread('../pictures/Small_Pass_Light2.png',-1)"""
-		self.frames = [frame3,frame4] #frame5,frame6,frame7,frame8]
-
-	""" Find the Ball Bearing and locate the centroid """
-	def findBB(self, img):
-
-		#img = cv2.imread(image,0)
-		img = cv2.medianBlur(img,5)
-		cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-
-		circles = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT,1,20, param1=50,param2=45,minRadius=80,maxRadius=0)
-
-		circles = np.uint16(np.around(circles))
-		for i in circles[0,:]:
-                    print "circle found"
-		    # draw the outer circle
-		    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-		    # draw the center of the circle
-		    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-
-		#cv2.imshow('detected circles',cimg)
-		#cv2.waitKey(0)
-		#cv2.destroyAllWindows()
-		return cimg
-
-	""" Find the Ball Bearing and locate the centroid """
-	def findBBCamera(self):
-
-		ret, self.frame = self.capture.read()
-
-		""" Convert BGR to HSV """
-		cimg = cv2.cvtColor(self.frame, cv2.COLOR_GRAY2BGR)
-
-		#ret, pic = self.capture.read()
-		#img = cv2.imread(pic,0)
-		#img = cv2.medianBlur(pic,5)
-		#cimg = cv2.cvtColor(pic, cv2.COLOR_GRAY2BGR)
-
-		circles = cv2.HoughCircles(self.frame, cv2.cv.CV_HOUGH_GRADIENT,1,20, param1=50,param2=45,minRadius=80,maxRadius=0)
-
-		circles = np.uint16(np.around(circles))
-		for i in circles[0,:]:
-		    # draw the outer circle
-		    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-		    # draw the center of the circle
-		    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-
-		cv2.imshow('detected circles',cimg)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
-		
-
-	""" Inspect the test array of images """
-	def inspectArray(self):
-
-                inspectArray = []
-
-		#for frame in self.frames:
-			#self.inspectImage(True,frame)
-                        #inspectArray.append(self.findBB(frame))
-                 #       self.findBB(frame)
-
-		if DEBUG:
-			while(True):
-				cnt = 0
-				for frame in self.frames:
-					cv2.imshow('Inspected Image ' + str(cnt),frame)
-					cnt += 1
-				key = cv2.waitKey(1) & 0xFF
-				if key == ord('q'):
-					break
-		
-		""" Close all windows currently open """
-		self.shutdown()
 
 	""" Called before inspecting a new blisk """
 	def newBlisk(self,bliskNum):
@@ -177,9 +88,9 @@ class ImageProcessor(object):
 
 			pic_count += 1
 
-                end_time =(time.time())
-                self.my_print("end_time: ")
-                self.my_print(end_time)
+		end_time =(time.time())
+		self.my_print("end_time: ")
+		self.my_print(end_time)
 		self.my_print("start_time: ")
 		self.my_print(start_time)
 		self.my_print("elapsed time: ")
@@ -188,96 +99,30 @@ class ImageProcessor(object):
 		self.my_print(pic_count)
 		self.my_print("fps: ")
 		self.my_print(pic_count/(end_time - start_time))
-		
-
 
 
 	""" Inspect Camera image """
 	def inspectCameraImage(self):
 
-                image_count = 1
-
+		image_count = 1
 		while(True):
 
 			""" Inspect the captured image """
 			passValue = self.inspectImageFromCamera(False)
-
 			#self.findBBCamera()
 
 			cv2.imshow('Inspected Camera Image ',self.frame)
-
 			key = cv2.waitKey(1) & 0xFF
 			if key == ord('q'):
 				break
 			elif key == ord('s'):
-                                image_count += 1
-                                img = self.frame
-                                cv2.imwrite("../pictures/Capture_" + str(image_count) +".png", img)
+				image_count += 1
+				img = self.frame
+				cv2.imwrite("../pictures/Capture_" + str(image_count) +".png", img)
 		
 		""" Close all windows currently open """
 		self.shutdown()
 
-
-	""" Inspect the current image to see if it passes """
-	def inspectImage(self, isSmallBB, frame): #, currStage, sizeBB):
-
-		imagePasses = False
-		frame_init = frame.copy()
-
-		""" Convert BGR to HSV """
-		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-		""" Threshold the HSV image to get only green colors """
-		mask = cv2.inRange(hsv, self.lower_green, self.upper_green)
-
-		""" Bitwise-AND mask and original image """
-		res = cv2.bitwise_and(frame,frame, mask= mask)
-
-		""" Find the Contours in the Mask """
-		(cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-		""" Initialize the counts of contours in each section to zero """
-		quad1_cnt = 0
-		quad2_cnt = 0
-		quad3_cnt = 0
-		 
-		""" Iterate over each of the contours """
-		for c in cnts:
-
-			""" compute the center of the contour """
-			M = cv2.moments(c)
-			area = M["m00"]
-			if M["m00"] == 0:
-				area = 0.000001
-			cX = int(M["m10"] / area)
-			cY = int(M["m01"] / area)
-
-			if DEBUG:
-				""" draw the contour and center of the shape on the image """
-				cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
-				cv2.circle(frame, (cX, cY), 7, (0, 0, 0), -1)
-
-				""" Draw the Boxes around each of the areas """
-				self.box1.draw(frame)
-				self.box2.draw(frame)
-				self.box3.draw(frame)
-
-			""" Determine the number of centroids in each quadrant """
-			if self.box1.inBox(cX,cY):
-				quad1_cnt += 1
-			elif self.box2.inBox(cX,cY):
-				quad2_cnt += 1
-			elif self.box3.inBox(cX,cY):
-				quad3_cnt += 1
-
-		""" Determine if the ball bearing case sizes pass """
-		imagePasses = self.checkImage(isSmallBB,quad1_cnt,quad2_cnt,quad3_cnt)
-
-                if(DEBUG):
-                        """ Indicate Whether the image passes or fails """
-                        cv2.putText(frame, "Inspection Passes: " + str(imagePasses), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.upper_green, 2)
-        
-		return imagePasses
 
 	""" Determine if the centroid count in each quadrant passes """
 	def checkImage(self,isSmallBB,quad1_cnt,quad2_cnt,quad3_cnt):
@@ -301,7 +146,6 @@ class ImageProcessor(object):
 			return True
 		return False	
 
-
 	""" When everything done, release the capture """
 	def shutdown(self):
 		if CAMERA:
@@ -310,12 +154,10 @@ class ImageProcessor(object):
 		if CAMERA:
 			self.capture.release()
 
-
 	""" Inspect the current image to see if it passes """
-	def inspectImageFromCamera(self, isSmallBB): #, currStage, sizeBB):
+	def inspectImageFromCamera(self, isSmallBB):
 
 		imagePasses = False
-		#frame_init = frame.copy()
 		ret, self.frame = self.capture.read()
 
 		""" Convert BGR to HSV """
@@ -370,26 +212,117 @@ class ImageProcessor(object):
 		""" Determine if the ball bearing case sizes pass """
 		imagePasses = self.checkImage(isSmallBB,quad1_cnt,quad2_cnt,quad3_cnt)
 
-                if(DEBUG):
-                        """ Indicate Whether the image passes or fails """
-                        cv2.putText(self.frame, "Inspection Passes: " + str(imagePasses), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.upper_green, 2)
+		if(DEBUG):
+			 """ Indicate Whether the image passes or fails """
+			 cv2.putText(self.frame, "Inspection Passes: " + str(imagePasses), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.upper_green, 2)
 
 		return imagePasses
 
-	def test(self):
 
-                cap = self.capture
+	def test3(isSmallBB):
 
-                while(cap.isOpened()):
-                        ret, frame = cap.read()
+		x_pos = 370
+		y_pos = 156
 
-                        cv2.imshow('frame', frame)
-                        key = cv2.waitKey(1) & 0xFF
-			if key == ord('q'):
+		if VIDEO:
+			video = cv2.VideoWriter('output.avi',-1, 7, (640,480))
+
+		pic_count = 0
+		start_time = time.time()
+
+		while(True):
+
+			ret, frame = cap.read()
+
+			pic_count += 1
+					
+			output = frame.copy()
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			
+			""" Apply GuassianBlur to reduce noise. medianBlur is also added for smoothening, reducing noise """
+			gray = cv2.GaussianBlur(gray,(1,1),100,100, 0);
+			gray = cv2.medianBlur(gray,1)
+			
+			""" Adaptive Guassian Threshold is to detect sharp edges in the Image """
+			gray = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,27,13.5)#29,15.5) working version
+			
+
+			# cv2.HoughCircles(image, method, dp (was 5), minDist (260?)[, circles[, param1[, param2 (65?)[, minRadius[, maxRadius]]]]])
+			if isSmallBB:
+				circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1, 4, param1=30, param2=25, minRadius=40, maxRadius=0)
+			else:
+				circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1, 1, param1=30, param2=25, minRadius=40, maxRadius=0)
+			
+			# ensure at least some circles were found
+			if circles is not None:
+				# convert the (x, y) coordinates and radius of the circles to integers
+				circles = np.round(circles[0, :]).astype("int")
+				
+				# loop over the (x, y) coordinates and radius of the circles
+				for (x, y, r) in circles:
+					# draw the circle in the output image, then draw a rectangle in the image
+					# corresponding to the center of the circle
+					#if(r > 70): (for larger bb)
+					if isSmallBB:
+
+						if(r > MIN_RAD_SMALL) and (r < MAX_RAD_SMALL) and (y<Y_MAX) and (y>Y_MIN) and (x<X_MAX) and (x>X_MIN): 
+							cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+							cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+							#time.sleep(0.5)
+							print "x: " + str(x)
+							print "y: " + str(y)
+							my_print("Radius is: ")
+							my_print(r)
+							my_print("\n")
+
+
+					elif (r > MIN_RAD_LARGE) and (r < MAX_RAD_LARGE) and (y<Y_MAX) and (y>Y_MIN) and (x<X_MAX) and (x>X_MIN):
+							
+						#cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+						#cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+						#time.sleep(0.5)
+						my_print("x: " + str(x) + '\n')
+						my_print("y: " + str(y) + '\n')				
+						my_print("Radius is: ")
+						my_print(r)
+						my_print("\n")
+
+						""" Adjust the ball pos based on the received values """
+						if(x != x_pos):
+							x_pos = ((WEIGHT) * x_pos + x)/(WEIGHT+1) 
+						if(y != y_pos):
+							y_pos = ((WEIGHT) * y_pos + y)/(WEIGHT+1) 
+
+			""" Show expected circle """
+			if isSmallBB:
+				cv2.circle(output, (370, 156), 64, (255, 0, 0), 4)
+				#cv2.rectangle(output, (X_MIN - 80, Y_MIN - 50), (X_MAX + 80, Y_MAX + 50), (0, 128, 255), 1)
+			else:
+				#cv2.circle(output, (370, 156), 74, (255, 0, 0), 4)
+				cv2.circle(output, (x_pos, y_pos), 74, (255, 0, 0), 4)
+				#cv2.rectangle(output, (X_MIN - 80, Y_MIN - 50), (X_MAX + 80, Y_MAX + 50), (0, 128, 255), 1)
+
+
+			video.write(output) #125 * np.ones((100,100,3), np.uint8)) 
+
+			# Display the resulting frame
+			#cv2.imshow('gray',gray)
+			cv2.imshow('frame',output)
+		 	if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
 
+		# When everything done, release the capture
+		end_time = time.time()
+		elapsed_time = end_time - start_time
+		print "number of frames: " + str(pic_count)
+		print "elapsed time: " + str(elapsed_time)
+		print "fps: " 
+		print pic_count/elapsed_time
+
+		video.release()
 		cap.release()
 		cv2.destroyAllWindows()
+
 
 	""" Function to handle system prints """
 	def my_print(self, text):
@@ -397,78 +330,16 @@ class ImageProcessor(object):
 	    sys.stdout.flush()
 
 
-""" Box Object to Store 4 points, draw the box and determine if points are within the box """
-class Box(object):
-
-	def __init__(self, x_pos, y_pos, x_width, y_width, filled):
-
-		""" Store the parameter values """
-		self.xPos = x_pos
-		self.yPos = y_pos
-		self.xWidth = x_width
-		self.yWidth = y_width
-		self.filled = filled
-
-		if(self.filled):
-                        """ Color to draw the box lines """
-                        self.color = (255,255,255)
-                else:
-                        self.color = (0, 0, 255)
-
-	""" Return the Upper Left Hand Point on the Box """
-	def p00(self):
-		return (self.xPos,self.yPos)
-
-	""" Return the Lower Left Hand Point on the Box """
-	def p01(self):
-		return (self.xPos,self.yPos + self.yWidth)
-
-	""" Return the Upper Right Hand Point on the Box """
-	def p10(self):
-		return (self.xPos + self.xWidth, self.yPos)
-
-	""" Return the Lower Right Hand Point on the Box """
-	def p11(self):
-		return (self.xPos + self.xWidth, self.yPos + self.yWidth)
-
-	""" Returns a Boolean Indicating whether the given x,y coordinates are within the box """
-	def inBox(self, x_pos, y_pos):
-
-		""" Check that the x and y values are in the correct range """
-		if((x_pos >= self.xPos) and (x_pos <= (self.xPos + self.xWidth))):
-			if((y_pos >= self.yPos) and (y_pos <= (self.yPos + self.yWidth))):
-				return 1
-
-		""" Otherwise return False """
-		return 0
-
-	""" Draw the Box from the Points on the given image """
-	def draw(self, frame):
-
-		cv2.line(frame, self.p00(), self.p01(), self.color)
-		cv2.line(frame, self.p00(), self.p10(), self.color)
-		cv2.line(frame, self.p11(), self.p01(), self.color)
-		cv2.line(frame, self.p11(), self.p10(), self.color)
 
 
 """ Used for testing purposes """
 if __name__ == "__main__":
 
 	ip = ImageProcessor()
-	#ip.test()
 	ip.inspectCameraImage()
-	ip.inspectArray()
-        #ip.findBBCamera()
+	#ip.findBBCamera()
 
-	"""ip.findBB('..\\better_pics\\Up2Covered.jpg')
-	ip.findBB('..\\better_pics\\Up2Uncovered.jpg')
-	ip.findBB('..\\better_pics\\Up1Uncovered.jpg')
-	ip.findBB('..\\better_pics\\Up1Covered.jpg')"""
 
-	"""results = InspectionResults.InspectionResults()
-	results.openNewFile("22")
-	results.addResult(1,2,3,4,'yo')
-	results.closeFile()"""
 
 
 
---
+++ entityClasses/Shapes.py
@@ -0,0 +1,56 @@
+
+
+
+
+""" Box Object to Store 4 points, draw the box and determine if points are within the box """
+class Box(object):
+
+	def __init__(self, x_pos, y_pos, x_width, y_width, filled):
+
+		""" Store the parameter values """
+		self.xPos = x_pos
+		self.yPos = y_pos
+		self.xWidth = x_width
+		self.yWidth = y_width
+		self.filled = filled
+
+		if(self.filled):
+			""" Color to draw the box lines """
+			self.color = (255,255,255)
+		else:
+			self.color = (0, 0, 255)
+
+	""" Return the Upper Left Hand Point on the Box """
+	def p00(self):
+		return (self.xPos,self.yPos)
+
+	""" Return the Lower Left Hand Point on the Box """
+	def p01(self):
+		return (self.xPos,self.yPos + self.yWidth)
+
+	""" Return the Upper Right Hand Point on the Box """
+	def p10(self):
+		return (self.xPos + self.xWidth, self.yPos)
+
+	""" Return the Lower Right Hand Point on the Box """
+	def p11(self):
+		return (self.xPos + self.xWidth, self.yPos + self.yWidth)
+
+	""" Returns a Boolean Indicating whether the given x,y coordinates are within the box """
+	def inBox(self, x_pos, y_pos):
+
+		""" Check that the x and y values are in the correct range """
+		if((x_pos >= self.xPos) and (x_pos <= (self.xPos + self.xWidth))):
+			if((y_pos >= self.yPos) and (y_pos <= (self.yPos + self.yWidth))):
+				return 1
+
+		""" Otherwise return False """
+		return 0
+
+	""" Draw the Box from the Points on the given image """
+	def draw(self, frame):
+
+		cv2.line(frame, self.p00(), self.p01(), self.color)
+		cv2.line(frame, self.p00(), self.p10(), self.color)
+		cv2.line(frame, self.p11(), self.p01(), self.color)
+		cv2.line(frame, self.p11(), self.p10(), self.color)
